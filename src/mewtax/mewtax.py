c67feba150ec2fd14e43108ff5194b013f03f478
Martin Schubert <mfschubert@gmail.com>
2024-02-09 14:15:35 -0800

    Add regularization

--- src/mewtax/mewtax.py
+++ src/mewtax/mewtax.py
@@ -20,6 +20,7 @@ def minimize_newton(
     z_init: PyTree,
     tol: float = 1e-5,
     max_iter: int = 20,
+    eps: float = 1e-8,
 ) -> PyTree:
     """Minimizes `fn` using the Newton method.
 
@@ -40,16 +41,28 @@ def minimize_newton(
         max_iter: Maximum number of iterations for the solver. No more than this
             number of iterations is performed, regardless of whether the minimization
             has converged. Default value is 20.
+        eps: Small positive value used to regularize the minimization problem.
 
     Returns:
         The pytree argmin of the function.
     """
-    z_init_flat, unflatten_z_fn = flatten_util.ravel_pytree(
+    z_init_flat, _unflatten_z_fn = flatten_util.ravel_pytree(
         z_init
     )  # type: ignore[no-untyped-call]
 
-    def flat_fn(params: jnp.ndarray, z_flat: jnp.ndarray) -> jnp.ndarray:
-        return fn(params, unflatten_z_fn(z_flat))
+    is_complex = jnp.iscomplexobj(z_init_flat)
+    if is_complex:
+        z_init_flat = jnp.concatenate([z_init_flat.real, z_init_flat.imag])
+
+    def unflatten_z_fn(z_flat):
+        if not is_complex:
+            return _unflatten_z_fn(z_flat)
+        z_flat_real, z_flat_imag = jnp.split(z_flat, 2)
+        z_flat = z_flat_real + 1j * z_flat_imag
+        return _unflatten_z_fn(z_flat)
+
+    def flat_fn(params: PyTree, z_flat: jnp.ndarray) -> jnp.ndarray:
+        return fn(params, unflatten_z_fn(z_flat)) + eps * jnp.linalg.norm(z_flat)**2
 
     z_flat = _minimize_newton(
         flat_fn,
@@ -87,7 +100,7 @@ def _minimize_newton(
 
     def fixed_point_fn(z: jnp.ndarray) -> jnp.ndarray:
         grad: jnp.ndarray = jax.grad(fn, argnums=1)(a, z)
-        return z - grad.conj()
+        return z - grad
 
     return _newton_solve_fixed_point(
         fixed_point_fn,
@@ -172,15 +185,14 @@ def _newton_solve_fixed_point(
     max_iter: int,
 ) -> jnp.ndarray:
     """Solves for a fixed point of `fn` using the Newton method."""
-    holomorphic = jnp.iscomplexobj(z_init)
+    assert not jnp.iscomplexobj(z_init)
 
     def root_fn(z: jnp.ndarray) -> jnp.ndarray:
         return fn(z) - z
 
     def g_fn(z: jnp.ndarray) -> jnp.ndarray:
-        jac = jax.jacfwd(root_fn, holomorphic=holomorphic)(z)
+        jac = jax.jacfwd(root_fn, holomorphic=False)(z)
         z_next: jnp.ndarray = z - jnp.linalg.solve(jac, root_fn(z))
-        is_problematic_jac = jnp.allclose(jac, 0)
-        return jnp.where(is_problematic_jac, root_fn(z), z_next)
+        return z_next
 
     return _fwd_solve_fixed_point(g_fn, z_init, tol, max_iter)
--- tests/test_mewtax.py
+++ tests/test_mewtax.py
@@ -4,6 +4,8 @@ import itertools
 import unittest
 
 import jax
+
+jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import numpy as onp
 import pytest
@@ -13,15 +15,18 @@ from parameterized import parameterized
 import mewtax
 
 
-def minimize_newton_naive(fn, params, z_init, tol=1e-5, max_iter=20):
+def minimize_newton_naive(fn, params, z_init, tol=1e-5, max_iter=20, eps=1e-8):
     """A naive implementation of the Newton method."""
+    def regularized_fn(params, z):
+        return fn(params, z) + eps * jnp.linalg.norm(z) ** 2
+
     z, unflatten_z_fn = flatten_util.ravel_pytree(z_init)
     params, _ = flatten_util.ravel_pytree(params)
     for _ in range(max_iter):
         grad = jax.grad(fn, argnums=1)(params, z)
         holomorphic = jnp.iscomplexobj(grad)
         hessian = jax.jacfwd(
-            jax.jacrev(fn, argnums=1), argnums=1, holomorphic=holomorphic
+            jax.jacrev(regularized_fn, argnums=1), argnums=1, holomorphic=holomorphic
         )(params, z)
         delta = jnp.linalg.solve(hessian, grad.conj())
         if not (jnp.linalg.norm(delta) > tol):
@@ -52,9 +57,20 @@ def quadratic_loss_fn(params, z):
     return jnp.sum(jnp.asarray(tree_util.tree_leaves(loss_tree)))
 
 
-def quartic_loss_fn(params, z):
-    loss_tree = tree_util.tree_map(lambda a, b: jnp.sum(jnp.abs(a - b) ** 4), params, z)
+def mixed_loss_fn(params, z):
+    loss_tree = tree_util.tree_map(
+        lambda a, b: jnp.sum(jnp.abs(a - b) ** 2 + jnp.abs(a - b) ** 4),
+        params,
+        z,
+    )
     return jnp.sum(jnp.asarray(tree_util.tree_leaves(loss_tree)))
+
+
+def _random_normal_like(key, x):
+    if jnp.iscomplexobj(x):
+        real, imag = jax.random.normal(key, (2,) + x.shape)
+        return real + 1j * imag
+    return jax.random.normal(key, x.shape)
 
 
 class MinimizeNewtonTest(unittest.TestCase):
@@ -71,10 +87,14 @@ class MinimizeNewtonTest(unittest.TestCase):
                 },
             ],
             # Quartic loss functions have more error, and we use looser test tolerances.
-            [quartic_loss_fn, jnp.arange(10).astype(float), 5e-3],
-            [quartic_loss_fn, jnp.arange(30).reshape(2, 5, 3).astype(float), 5e-3],
+            [mixed_loss_fn, jnp.arange(10).astype(float), 5e-3],
             [
-                quartic_loss_fn,
+                mixed_loss_fn,
+                jnp.arange(30).reshape(2, 5, 3).astype(float),
+                5e-3,
+            ],
+            [
+                mixed_loss_fn,
                 {
                     "a": jnp.arange(5).astype(float),
                     "b": jnp.arange(5, 10).astype(float),
@@ -91,11 +111,7 @@ class MinimizeNewtonTest(unittest.TestCase):
                 jax.random.PRNGKey(0), num=len(tree_util.tree_leaves(params))
             ),
         )
-        z_init = tree_util.tree_map(
-            lambda k, p: jax.random.normal(k, p.shape).astype(p.dtype),
-            keys,
-            params,
-        )
+        z_init = tree_util.tree_map(_random_normal_like, keys, params)
         z_star = mewtax.minimize_newton(fn=loss_fn, params=params, z_init=z_init)
         for a, b in zip(tree_util.tree_leaves(z_star), tree_util.tree_leaves(params)):
             onp.testing.assert_allclose(a, b, rtol=tol, atol=tol)
@@ -105,9 +121,9 @@ class MinimizeNewtonTest(unittest.TestCase):
             [quadratic_loss_fn, jnp.arange(10) * (1 + 0j)],
             [quadratic_loss_fn, jnp.arange(10) * (0 + 1j)],
             [quadratic_loss_fn, jnp.arange(10) * 1j],
-            [quartic_loss_fn, jnp.arange(10) * (1 + 0j), 5e-3],
-            [quartic_loss_fn, jnp.arange(10) * (0 + 1j), 5e-3],
-            [quartic_loss_fn, jnp.arange(10) * (1 + 1j), 5e-3],
+            [mixed_loss_fn, jnp.arange(10) * (1 + 0j), 5e-3],
+            [mixed_loss_fn, jnp.arange(10) * (0 + 1j), 5e-3],
+            [mixed_loss_fn, jnp.arange(10) * (1 + 1j), 5e-3],
         ]
     )
     def test_solution_matches_expected_complex(self, loss_fn, params, tol=1e-6):
@@ -117,12 +133,10 @@ class MinimizeNewtonTest(unittest.TestCase):
                 jax.random.PRNGKey(0), num=len(tree_util.tree_leaves(params))
             ),
         )
-        z_init = tree_util.tree_map(
-            lambda k, p: jax.random.normal(k, p.shape).astype(p.dtype),
-            keys,
-            params,
+        z_init = tree_util.tree_map(_random_normal_like, keys, params)
+        z_star = mewtax.minimize_newton(
+            fn=loss_fn, params=params, z_init=z_init, tol=1e-5, max_iter=100
         )
-        z_star = mewtax.minimize_newton(fn=loss_fn, params=params, z_init=z_init)
         for a, b in zip(tree_util.tree_leaves(z_star), tree_util.tree_leaves(params)):
             onp.testing.assert_allclose(a, b, rtol=tol, atol=tol)
 
@@ -138,10 +152,10 @@ class MinimizeNewtonTest(unittest.TestCase):
                     "c": jnp.arange(10, 20).reshape(2, 5).astype(float),
                 },
             ],
-            [quartic_loss_fn, jnp.arange(10).astype(float)],
-            [quartic_loss_fn, jnp.arange(30).reshape(2, 5, 3).astype(float)],
+            [mixed_loss_fn, jnp.arange(10).astype(float)],
+            [mixed_loss_fn, jnp.arange(30).reshape(2, 5, 3).astype(float)],
             [
-                quartic_loss_fn,
+                mixed_loss_fn,
                 {
                     "a": jnp.arange(5).astype(float),
                     "b": jnp.arange(5, 10).astype(float),
@@ -150,7 +164,6 @@ class MinimizeNewtonTest(unittest.TestCase):
             ],
         ]
     )
-    @pytest.mark.xfail
     def test_gradient_matches_naive_real(self, loss_fn, params):
         keys = tree_util.tree_unflatten(
             tree_util.tree_structure(params),
@@ -158,11 +171,7 @@ class MinimizeNewtonTest(unittest.TestCase):
                 jax.random.PRNGKey(0), num=len(tree_util.tree_leaves(params))
             ),
         )
-        z_init = tree_util.tree_map(
-            lambda k, p: jax.random.normal(k, p.shape).astype(p.dtype),
-            keys,
-            params,
-        )
+        z_init = tree_util.tree_map(_random_normal_like, keys, params)
 
         def meta_loss_fn(params):
             z_star = mewtax.minimize_newton(fn=loss_fn, params=params, z_init=z_init)
@@ -190,12 +199,11 @@ class MinimizeNewtonTest(unittest.TestCase):
             [quadratic_loss_fn, jnp.arange(10) * (1 + 0j)],
             [quadratic_loss_fn, jnp.arange(10) * (0 + 1j)],
             [quadratic_loss_fn, jnp.arange(10) * (1 + 1j)],
-            [quartic_loss_fn, jnp.arange(10) * (1 + 0j)],
-            [quartic_loss_fn, jnp.arange(10) * (0 + 1j)],
-            [quartic_loss_fn, jnp.arange(10) * (1 + 1j)],
+            [mixed_loss_fn, jnp.arange(10) * (1 + 0j)],
+            [mixed_loss_fn, jnp.arange(10) * (0 + 1j)],
+            [mixed_loss_fn, jnp.arange(10) * (1 + 1j)],
         ]
     )
-    @pytest.mark.xfail
     def test_gradient_matches_naive_complex(self, loss_fn, params):
         keys = tree_util.tree_unflatten(
             tree_util.tree_structure(params),
@@ -203,11 +211,7 @@ class MinimizeNewtonTest(unittest.TestCase):
                 jax.random.PRNGKey(0), num=len(tree_util.tree_leaves(params))
             ),
         )
-        z_init = tree_util.tree_map(
-            lambda k, p: jax.random.normal(k, p.shape).astype(p.dtype),
-            keys,
-            params,
-        )
+        z_init = tree_util.tree_map(_random_normal_like, keys, params)
 
         def meta_loss_fn(params):
             z_star = mewtax.minimize_newton(fn=loss_fn, params=params, z_init=z_init)
@@ -221,6 +225,8 @@ class MinimizeNewtonTest(unittest.TestCase):
 
         value, grad = jax.value_and_grad(meta_loss_fn)(params)
         naive_value, naive_grad = jax.value_and_grad(naive_meta_loss_fn)(params)
+
+        print(value, naive_value)
 
         params_flat, _ = flatten_util.ravel_pytree(params)
         expected_value = jnp.sum(jnp.abs(params_flat) ** 2)
@@ -254,7 +260,6 @@ class MinimizeNewtonTest(unittest.TestCase):
             [(jnp.arange(4) - 1.5) * (0.1 + 0.1j)],
         ]
     )
-    @pytest.mark.xfail
     def test_gradient_matches_finite_difference_complex(self, params):
         # Verifies that the Newton gradients match finite difference gradients.
         def loss_fn(params, x):
@@ -300,7 +305,7 @@ class MinimizeNewtonTest(unittest.TestCase):
             return jnp.sum(jnp.abs(x - params) ** 2)
 
         def fn(z_init):
-            return minimize_newton_naive(
+            return mewtax.minimize_newton(
                 fn=loss_fn,
                 params=jnp.arange(4).astype(float),
                 z_init=z_init,
