1d00294727390fe1722a78e4a201c5b3c135191a
Martin Schubert <mfschubert@gmail.com>
2024-06-14 12:52:13 -0700

    Version updated from v0.0.0 to v0.0.1

--- src/mewtax/__init__.py
+++ src/mewtax/__init__.py
@@ -1,6 +1,6 @@
 """mewtax - Differentiable minimization in jax using Newton's method."""
 
-__version__ = "v0.0.0"
+__version__ = "v0.0.1"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 from mewtax.mewtax import minimize_newton as minimize_newton
